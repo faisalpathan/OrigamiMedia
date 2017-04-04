2533ec3f9c7f532ed15c6ab2ce85597f8a91c450
FaisalKhan <FaisalKhan>
2017-04-04 16:57:02 +0530

    uploading style change

--- resources/assets/js/components/VideoUpload.vue
+++ resources/assets/js/components/VideoUpload.vue
@@ -6,7 +6,19 @@
                 <div class="panel-heading">Upload</div>
                 <form>
                 <div class="panel-body">
-                    <input type="file" name="video" id="video" @change="fileInputChange" v-if="!uploading">
+                    <!-- <input type="file" name="video" id="video" @change="fileInputChange" v-if="!uploading" accept= "video/*"> -->
+                <div class="fileinput fileinput-new input-group" data-provides="fileinput" v-if="!uploading">
+
+                        <div class="form-control" data-trigger="fileinput"><i class="glyphicon glyphicon-file fileinput-exists"></i><span class="fileinput-filename"></span>
+                        </div>
+
+                        <span class="input-group-addon btn btn-default btn-file"><span class="fileinput-new">Select Video File</span><span class="fileinput-exists">Change</span>
+                        <input type="file" name="video" id="video" @change="fileInputChange" 
+                         accept= "video/*">
+                        </span>
+                        <a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
+                
+                </div>
                 </form>
                     <div class="alert alert-danger" v-if="failed">Something Failed Please Try again.
                     
